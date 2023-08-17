27980d01f92c4235562bc17aba252763ae45202a
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-08-17 19:27:23 +0530

    basic programming in javascript

---
+++ multiplicationTableOf7.js
@@ -0,0 +1,6 @@
+let i;
+for(i=1; i<=10; i++)
+{
+    let table = "7 * " + i + " = " + 7*i;
+    console.log(table);  
+}
---
+++ sumOFNumbers.js
@@ -0,0 +1,7 @@
+let sum=0;
+for(i=1;i<=10;i++)
+{
+    sum=sum+i;
+
+}
+console.log(sum);
