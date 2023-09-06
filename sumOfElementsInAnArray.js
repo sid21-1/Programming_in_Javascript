0685c73dd9b25cf527c93bcd3593e35a6624f745
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-09-06 13:37:20 +0530

    sum of elements in an array

---
+++ sumOfElementsInAnArray.js
@@ -0,0 +1,10 @@
+let sum =0;
+let array =[10,20,30,40,50]
+array.forEach(functionsum)
+
+function functionsum(element){
+    sum = sum +element;
+    // console.log(sum); 
+}
+
+console.log("the final sum of the elements in the given array is " + sum);
