929458a3809e5fdd070f2e22011f14987fc2a682
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-09-11 15:42:51 +0530

    sorting an array

---
+++ sortingAnArray.js
@@ -0,0 +1,10 @@
+const array=[23,1,4,5,2,34]
+sortArray(array)
+
+function sortArray(arr){
+    arr.sort();
+}
+
+console.log(array);
+
+//Remember the array sorts the numbers in the form of a string so you have to convert it into number
