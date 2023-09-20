84e98d2a8ec9ad1fd1444391735ea5566be8e1d9
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-09-20 14:35:14 +0530

    even Odd program

---
+++ If-else questions/evenOdd.js
@@ -0,0 +1,10 @@
+function evenOdd(number){
+    if(number % 2 === 0){
+        console.log("it is a even number")
+    }
+    else{
+        console.log("it is an odd number")
+    }
+}
+
+evenOdd(12);
