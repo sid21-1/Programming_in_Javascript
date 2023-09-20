81283c1718d519a4cb78182851d1012a9f2460c4
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-09-20 14:37:48 +0530

    positive negative number

---
+++ If-else questions/posNeg.js
@@ -0,0 +1,11 @@
+function checknumber(number){
+    if (number>0){
+        console.log("it is a positive numnber");
+    }
+    else {
+        console.log("it is a negative number");
+    }
+}
+
+checknumber(12);
+checknumber(-2);
