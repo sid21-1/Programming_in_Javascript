0393b0e2181f59d907efb13748de942f322bfc85
Siddhanth Gupta <guptasiddhanth2@gmail.com>
2023-10-15 13:47:04 +0530

    vowels in a string

---
+++ If-else questions/vowels.js
@@ -0,0 +1,13 @@
+let str = "helloworld";
+
+function countVowels(){
+    let count =0;
+    for (let i =0; i< str.length; i++){
+        if(str.charAt(i)=="a"||str.charAt(i)=="e"||str.charAt(i)=="i"||str.charAt(i)=="o"||str.charAt(i)=="u"){
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+countVowels();
