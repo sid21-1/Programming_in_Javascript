b9587afaf0036b992fb74893993de28e1bdf4846
Siddhanth Gupta <90953782+sid21-1@users.noreply.github.com>
2023-10-29 12:52:09 +0530

    level one and button flash

--- javascript games/Simon game/app.js
+++ javascript games/Simon game/app.js
@@ -1,12 +1,39 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["yellow", "red", "purple", "green"];
+
 let started = false;
 let level = 0;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function () {
   if (started == false) {
     console.log("game started");
     started = "true";
+
+    levelUp();
   }
 });
+
+function btnFlash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
+}
+
+function levelUp() {
+  level++;
+  h2.innerText = `level ${level}`;
+
+  let randomIdx = Math.floor(Math.random() * 3);
+  let randColor = btns[randomIdx];
+  let ranBtn = document.querySelector(`.${randColor}`);
+  console.log(randomIdx);
+  console.log(randColor);
+  console.log(ranBtn);
+
+  btnFlash(ranBtn);
+}
