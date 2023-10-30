777e618de92359b91c37b78302446a799547dda8
Siddhanth Gupta <90953782+sid21-1@users.noreply.github.com>
2023-10-30 09:22:44 +0530

    matching sequence for the game

--- javascript games/Simon game/app.js
+++ javascript games/Simon game/app.js
@@ -32,23 +32,41 @@ function userFlash(btn) {
 }
 
 function levelUp() {
+  userSeq = [];
   level++;
   h2.innerText = `level ${level}`;
 
   let randomIdx = Math.floor(Math.random() * 3);
   let randColor = btns[randomIdx];
   let ranBtn = document.querySelector(`.${randColor}`);
-  console.log(randomIdx);
-  console.log(randColor);
-  console.log(ranBtn);
+  // console.log(randomIdx);
+  // console.log(randColor);
+  // console.log(ranBtn);
+  gameSeq.push(randColor);
+  console.log(gameSeq);
 
   gameFlash(ranBtn);
+}
+
+function checkAnswer(idx) {
+  // console.log("curr Level", level)
+  if (userSeq[idx] == gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerText = `Game Over! Press any key to start`;
+  }
 }
 
 function btnPress() {
   console.log(this);
   let btn = this;
   userFlash(btn);
+  userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+
+  checkAnswer(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
