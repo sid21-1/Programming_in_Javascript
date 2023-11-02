484e2eda19fba62294c468f135b69c3e7262ad0d
Siddhanth Gupta <90953782+sid21-1@users.noreply.github.com>
2023-11-02 11:39:08 +0530

    using async and await to make api calls

--- API's/first api request/script.js
+++ API's/first api request/script.js
@@ -1,20 +1,34 @@
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data.fact);
-    return fetch(url);
-  })
-  .then((response)=>{
-    return response.json();
-  })
-  .then((data)=>{
-    console.log(data.fact);
-  })
-  .catch((err) => {
-    console.log("error-", err);
-  });
+// fetch(url)
+//   .then((response) => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data.fact);
+//     return fetch(url);
+//   })
+//   .then((response)=>{
+//     return response.json();
+//   })
+//   .then((data)=>{
+//     console.log(data.fact);
+//   })
+//   .catch((err) => {
+//     console.log("error-", err);
+//   });
+
+async function getFacts(){
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(res);
+        console.log(data.fact);
+    }
+    catch (e){
+        console.log("error", e);
+    }
+}
+
+getFacts();
